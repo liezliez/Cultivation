826c630a059e52ef822978269c9ad6d3ec4a308e
liez <liezliez73@gmail.com>
2021-06-12 23:35:22 +0700

    delete edit user diimplementasi

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -49,11 +49,23 @@ class User extends BaseController
     {
         $data = [
             'id' => $id_user,
-            'username' => $this->request->getPost(),
-            'email' => $this->request->getPost(),
+            'username' => $this->request->getPost('username'),
+            'email' => $this->request->getPost('email'),
+            'role_id' => $this->request->getPost('role_id'),
+            'active' => $this->request->getPost('active'),
         ];
         $this->usersModel->editUser($data);
         session()->setFlashdata('edit', 'data berhasil diubah');
+        return redirect()->to(base_url('user'));
+    }
+
+    public function deleteUser($id_user)
+    {
+        $data = [
+            'id' => $id_user,
+        ];
+        $this->usersModel->deleteUser($data);
+        session()->setFlashdata('delete', 'data berhasil didelete');
         return redirect()->to(base_url('user'));
     }
 }
--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -8,61 +8,80 @@
 <!-- Content Section -->
 <?= $this->section('content'); ?>
 
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb__links">
+                    <a href="/"><i class="fa fa-home"></i> Homepage</a>
+                    <span>Daftar User</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
 
-<div class="col-12">
-    <div class="card text-white bg-dark">
-        <div class="card-header">
-            <h3 class="card-title" style="font-family: Oswald, sans-serif;">Data User</h3>
-            <div class="card-tools">
-                <div class="input-group input-group-sm" style="width: 450px;">
-                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                    <div class="input-group-append">
-                        <button type="submit" class="btn btn-secondary">Temukan
-                        </button>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="section-title">
+                    <h4 style="margin-top: 32px;">Daftar User</h4>
+                </div>
+                <div class="card text-white bg-dark">
+                    <div class="card-header">
+                        <!-- <h3 class="card-title" style="font-family: Oswald, sans-serif;">Data User</h3> -->
+                        <div class="card-tools">
+                            <div class="input-group input-group-md">
+                                <input type="text" name="table_search" class="form-control float-right" placeholder="Cari Username">
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-secondary">Temukan
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="col-11 card-body table-responsive p-0 ml-3">
+                        <table class="table table-striped table-dark table-head-fixed text-nowrap">
+                            <thead class="thead-dark ">
+                                <tr>
+                                    <th style="width: 70px;">No</th>
+                                    <th style="width: 70px;">ID</th>
+                                    <th>Username</th>
+                                    <th>Email</th>
+                                    <th>Role</th>
+                                    <th>Status</th>
+                                    <th>
+                                        Aksi
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $no = 1;
+                                foreach ($users as $key => $value) { ?>
+                                    <tr>
+                                        <td><?= $no++; ?></td>
+                                        <td><?= $value['id']; ?></td>
+                                        <td><?= $value['username']; ?></td>
+                                        <td><?= $value['email']; ?></td>
+                                        <td><?= ($value['role_id'] == 1) ? 'admin' : 'user'; ?></td>
+                                        <td><?= ($value['active'] == 1) ? 'aktif' : 'non-aktif'; ?></td>
+                                        <td>
+                                            <button class="btn btn-secondary" data-toggle="modal" data-target="#edit<?= $value['id']; ?>"><i class="fa fa-pencil"></i></button>
+                                            <button class="btn btn-secondary" data-toggle="modal" data-target="#delete<?= $value['id']; ?>"><i class="fa fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                <?php }  ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- /.card-header -->
-        <div class="col-9 card-body table-responsive p-0 ml-3">
-            <table class="table table-striped table-dark table-head-fixed text-nowrap">
-                <thead class="thead-dark ">
-                    <tr>
-                        <th style="width: 70px;">No</th>
-                        <th style="width: 70px;">ID</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Status</th>
-                        <th>
-                            Aksi
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $no = 1;
-                    foreach ($users as $key => $value) { ?>
-                        <tr>
-                            <td><?= $no++; ?></td>
-                            <td><?= $value['id']; ?></td>
-                            <td><?= $value['username']; ?></td>
-                            <td><?= $value['email']; ?></td>
-                            <td><?= ($value['role_id'] == 1) ? 'admin' : 'user'; ?></td>
-                            <td><?= ($value['active'] == 1) ? 'aktif' : 'non-aktif'; ?></td>
-                            <td>
-                                <button class="btn btn-secondary" data-toggle="modal" data-target="#edit<?= $value['id']; ?>"><i class="fa fa-pencil"></i></button>
-                                <button class="btn btn-secondary" data-toggle="modal" data-target="#delete<?= $value['id']; ?>"><i class="fa fa-trash"></i></button>
-                            </td>
-                        </tr>
-                    <?php }  ?>
-                </tbody>
-            </table>
-        </div>
-        <!-- /.card-body -->
     </div>
-    <!-- /.card -->
-
-    <!-- modal -->
 
     <!-- Modal Edit -->
     <?php foreach ($users as $key => $value) { ?>
@@ -95,10 +114,35 @@
                         </div>
                         <div class="modal-footer justify-content-between">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
+                            <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
                         </div>
                     </div>
                     <?php echo form_close() ?>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+    <?php }  ?>
+    <!-- Modal delete -->
+    <?php foreach ($users as $key => $value) { ?>
+        <div class="modal fade" id="delete<?= $value['id']; ?>">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Delete User</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Apakah anda ingin menghapus <b><?= $value['username']; ?></b> ?
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary btn-danger">Delete</button>
+                        </div>
+                    </div>
                 </div>
                 <!-- /.modal-content -->
             </div>
